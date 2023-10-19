e5803e7d1e0610715366d7318c504934ab5eac79
Luca G <xxlpmghdxx@gmail.com>
2023-10-19 14:40:45 +0200

    cleanup

--- docs_doxygen/html/search/all_1.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['dambreak1d_0',['dambreak1d',['../classtsunami__lab_1_1setups_1_1_dam_break1d.html',1,'tsunami_lab::setups::DamBreak1d'],['../classtsunami__lab_1_1setups_1_1_dam_break1d.html#a9d09ba838d9a6699b6e2821bc9ed08b7',1,'tsunami_lab::setups::DamBreak1d::DamBreak1d()']]],
-  ['dambreak1d_2ecpp_1',['DamBreak1d.cpp',['../_dam_break1d_8cpp.html',1,'']]],
-  ['dambreak1d_2eh_2',['DamBreak1d.h',['../_dam_break1d_8h.html',1,'']]],
-  ['dambreak1d_2etest_2ecpp_3',['DamBreak1d.test.cpp',['../_dam_break1d_8test_8cpp.html',1,'']]]
-];
--- docs_doxygen/html/search/files_6.js
+++
@@ -1,7 +0,0 @@
-var searchData=
-[
-  ['wavepropagation_2eh_0',['WavePropagation.h',['../_wave_propagation_8h.html',1,'']]],
-  ['wavepropagation1d_2ecpp_1',['WavePropagation1d.cpp',['../_wave_propagation1d_8cpp.html',1,'']]],
-  ['wavepropagation1d_2eh_2',['WavePropagation1d.h',['../_wave_propagation1d_8h.html',1,'']]],
-  ['wavepropagation1d_2etest_2ecpp_3',['WavePropagation1d.test.cpp',['../_wave_propagation1d_8test_8cpp.html',1,'']]]
-];
