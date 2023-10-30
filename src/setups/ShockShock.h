998f7b68b44071b638f55cf5a2706760ff380caa
ZeyxRew <117768526+ZeyxRew@users.noreply.github.com>
2023-10-30 15:17:19 +0100

    setup RareRare and ShockShock

---
+++ src/setups/ShockShock.h
@@ -0,0 +1,21 @@
+/**
+ * @author Maximilian Grumbacher & Richard Hofmann 
+ *
+ * @section DESCRIPTION
+ * Implementation of shock-shock Riemann problems
+ **/
+
+#ifndef TSUNAMI_LAB_SETUPS_SHOCK_SHOCK_H
+#define TSUNAMI_LAB_SETUPS_SHOCK_SHOCK_H
+
+#include "Setup.h"
+
+namespace tsunami_lab {
+  namespace setups {
+    class ShockShock;
+  }
+}
+
+
+
+#endif
