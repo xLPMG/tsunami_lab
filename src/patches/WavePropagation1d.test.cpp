dd4bd824d5cb7b0d4b750f66da755321380af510
Luca G <xxlpmghdxx@gmail.com>
2023-10-31 13:24:37 +0100

    preparation to read .csv file

--- src/patches/WavePropagation1d.test.cpp
+++ src/patches/WavePropagation1d.test.cpp
@@ -6,8 +6,11 @@
  **/
 #include <catch2/catch.hpp>
 #include "WavePropagation1d.h"
+#include <fstream>
+#include <sstream>
 
-TEST_CASE( "Test the 1d wave propagation solver.", "[WaveProp1d]" ) {
+TEST_CASE("Test the 1d wave propagation solver.", "[WaveProp1d]")
+{
   /*
    * Test case:
    *
@@ -26,47 +29,75 @@ TEST_CASE( "Test the 1d wave propagation solver.", "[WaveProp1d]" ) {
    */
 
   // construct solver and setup a dambreak problem
-  tsunami_lab::patches::WavePropagation1d m_waveProp( 100, "roe" );
+  tsunami_lab::patches::WavePropagation1d m_waveProp(100, "roe");
 
-  for( std::size_t l_ce = 0; l_ce < 50; l_ce++ ) {
-    m_waveProp.setHeight( l_ce,
-                          0,
-                          10 );
-    m_waveProp.setMomentumX( l_ce,
-                             0,
-                             0 );
+  for (std::size_t l_ce = 0; l_ce < 50; l_ce++)
+  {
+    m_waveProp.setHeight(l_ce,
+                         0,
+                         10);
+    m_waveProp.setMomentumX(l_ce,
+                            0,
+                            0);
   }
-  for( std::size_t l_ce = 50; l_ce < 100; l_ce++ ) {
-    m_waveProp.setHeight( l_ce,
-                          0,
-                          8 );
-    m_waveProp.setMomentumX( l_ce,
-                             0,
-                             0 );
+  for (std::size_t l_ce = 50; l_ce < 100; l_ce++)
+  {
+    m_waveProp.setHeight(l_ce,
+                         0,
+                         8);
+    m_waveProp.setMomentumX(l_ce,
+                            0,
+                            0);
   }
 
   // set outflow boundary condition
   m_waveProp.setGhostOutflow();
 
   // perform a time step
-  m_waveProp.timeStep( 0.1 );
+  m_waveProp.timeStep(0.1);
 
   // steady state
-  for( std::size_t l_ce = 0; l_ce < 49; l_ce++ ) {
-    REQUIRE( m_waveProp.getHeight()[l_ce]   == Approx(10) );
-    REQUIRE( m_waveProp.getMomentumX()[l_ce] == Approx(0) );
+  for (std::size_t l_ce = 0; l_ce < 49; l_ce++)
+  {
+    REQUIRE(m_waveProp.getHeight()[l_ce] == Approx(10));
+    REQUIRE(m_waveProp.getMomentumX()[l_ce] == Approx(0));
   }
 
   // dam-break
-  REQUIRE( m_waveProp.getHeight()[49]   == Approx(10 - 0.1 * 9.394671362) );
-  REQUIRE( m_waveProp.getMomentumX()[49] == Approx( 0 + 0.1 * 88.25985) );
+  REQUIRE(m_waveProp.getHeight()[49] == Approx(10 - 0.1 * 9.394671362));
+  REQUIRE(m_waveProp.getMomentumX()[49] == Approx(0 + 0.1 * 88.25985));
 
-  REQUIRE( m_waveProp.getHeight()[50]   == Approx(8 + 0.1 * 9.394671362) );
-  REQUIRE( m_waveProp.getMomentumX()[50] == Approx(0 + 0.1 * 88.25985) );
+  REQUIRE(m_waveProp.getHeight()[50] == Approx(8 + 0.1 * 9.394671362));
+  REQUIRE(m_waveProp.getMomentumX()[50] == Approx(0 + 0.1 * 88.25985));
 
   // steady state
-  for( std::size_t l_ce = 51; l_ce < 100; l_ce++ ) {
-    REQUIRE( m_waveProp.getHeight()[l_ce]   == Approx(8) );
-    REQUIRE( m_waveProp.getMomentumX()[l_ce] == Approx(0) );
+  for (std::size_t l_ce = 51; l_ce < 100; l_ce++)
+  {
+    REQUIRE(m_waveProp.getHeight()[l_ce] == Approx(8));
+    REQUIRE(m_waveProp.getMomentumX()[l_ce] == Approx(0));
   }
+}
+
+TEST_CASE("1d wave propagation solver sanitiy check", "[WaveProp1d_SanityCheck]")
+{
+  std::ofstream outputFile("out.txt");
+  std::ifstream inputFile("middle_states.csv");
+  std::vector<std::string> row;
+  std::string line, word;
+  while (getline(inputFile, line))
+  {
+    // ignore lines starting with #
+    if (line.substr(0, 1) != "#")
+    {
+      row.clear();
+      std::stringstream s(line);
+      while (getline(s, word, ','))
+      {
+        // add all the column data of a row to a vector
+        row.push_back(word);
+      }
+      outputFile << row[0] << " + " << row[1] << std::endl;
+    }
+  }
+  outputFile.close();
 }
