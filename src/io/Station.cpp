ac1520e867d48a1d19c4398c17df4a15a193a858
ZeyxRew <117768526+ZeyxRew@users.noreply.github.com>
2023-11-16 09:46:09 +0100

    Update Station.cpp

---
+++ src/io/Station.cpp
@@ -0,0 +1,17 @@
+/**
+ * @author Alexander Breuer (alex.breuer AT uni-jena.de)
+ *
+ * @section DESCRIPTION
+ * IO-routines for writing a snapshot as Comma Separated Values (CSV).
+ **/
+#include "Station.h"
+#include <sstream>
+
+tsunami_lab::io::Station::Station(t_real i_x,
+                                  t_real i_y,
+                                  std::string name,
+                                  t_real i_h,
+                                  t_real i_b)
+{
+    
+}
