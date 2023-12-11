aecdc92f11e6956f4d3ae8b663251a0007786412
ZeyxRew <117768526+ZeyxRew@users.noreply.github.com>
2023-12-11 14:23:58 +0100

    add sbatch

---
+++ sbatch/batch.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+#SBATCH --job-name=tohoku_1000
+#SBATCH --output=tohoku_1000.out
+#SBATCH --error=tohoku_1000.err
+#SBATCH --partition=s_hadoop
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=72
+
+
+
+# Enter your executable commands here
+# Execute the compiled program
+scons
+./build/tsunami_lab
