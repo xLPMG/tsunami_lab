1812b0548c9d790d81ba065f643848221fcad2cb
Luca G <xxlpmghdxx@gmail.com>
2023-12-12 22:32:46 +0100

    adde sbatch files for chile5000 and chile1000

---
+++ sbatch/batch_chile1000.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=chile_1000
+#SBATCH --output=chile_1000.out
+#SBATCH --error=chile_1000.err
+#SBATCH --partition=s_hadoop
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=72
+
+#load modules
+module load compiler/gcc/11.2.0
+
+# Enter your executable commands here
+# Execute the compiled program
+scons
+./build/tsunami_lab configs/chile1000.json
---
+++ sbatch/batch_chile5000.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+#SBATCH --job-name=chile_5000
+#SBATCH --output=chile_5000.out
+#SBATCH --error=chile_5000.err
+#SBATCH --partition=s_hadoop
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --time=10:00:00
+#SBATCH --cpus-per-task=72
+
+#load modules
+module load compiler/gcc/11.2.0
+
+# Enter your executable commands here
+# Execute the compiled program
+scons
+./build/tsunami_lab configs/chile5000.json
