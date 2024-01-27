3665e305b1e4b84141ffb7173e43bde36aba3cb2
ZeyxRew <117768526+ZeyxRew@users.noreply.github.com>
2024-01-27 20:38:42 +0100

    remove events

--- src/ui/GUI.h
+++ src/ui/GUI.h
@@ -68,7 +68,7 @@ private:
   char m_sbTim[256] = "10:00:00";
 
   // simulation parameters
-  const char *m_tsunamiEvents[5] = {"CUSTOM", "TOHOKU", "CHILE", "ARTIFICIAL2D","CIRCULARDAMBREAK2D"};
+  const char *m_tsunamiEvents[3] = {"CUSTOM", "ARTIFICIAL2D", "CIRCULARDAMBREAK2D"};
   int m_tsunamiEvent = 0;
   int m_nx = 1;
   int m_ny = 1;
