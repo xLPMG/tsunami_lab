28878ce50152c57811040e6ac6f7e47771088d11
ZeyxRew <117768526+ZeyxRew@users.noreply.github.com>
2024-02-04 17:36:14 +0100

    Update CircularDamBreak2d.test.cpp

--- src/setups/CircularDamBreak2d.test.cpp
+++ src/setups/CircularDamBreak2d.test.cpp
@@ -13,26 +13,26 @@ TEST_CASE("Test the two-dimensional dam break setup.", "[DamBreak2d]")
   tsunami_lab::setups::CircularDamBreak2d l_damBreak(10,5,10);
 
   // sqrt(x^2 +y^2) < 10
-  REQUIRE(l_damBreak.getHeight(0, 0) == 10);
+  REQUIRE(l_damBreak.getHeight(0, 0) == 110);
 
   REQUIRE(l_damBreak.getMomentumX(45, 45) == 0);
 
   REQUIRE(l_damBreak.getMomentumY(45, 45) == 0);
 
-  REQUIRE(l_damBreak.getHeight(2, 2) == 10);
+  REQUIRE(l_damBreak.getHeight(2, 2) == 110);
 
   REQUIRE(l_damBreak.getMomentumX(2, 2) == 0);
 
   REQUIRE(l_damBreak.getMomentumY(2, 2) == 0);
 
   // sqrt(x^2 +y^2) > 10
-  REQUIRE(l_damBreak.getHeight(10, 5) == 5);
+  REQUIRE(l_damBreak.getHeight(10, 5) == 105);
 
   REQUIRE(l_damBreak.getMomentumX(10, 5) == 0);
 
   REQUIRE(l_damBreak.getMomentumY(10, 5) == 0);
 
-  REQUIRE(l_damBreak.getHeight(15, 15) == 5);
+  REQUIRE(l_damBreak.getHeight(15, 15) == 105);
 
   REQUIRE(l_damBreak.getMomentumX(15, 15) == 0);
 
